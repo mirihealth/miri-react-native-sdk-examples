43fe50b184b273737e915d007fd6c9201b7789ce
shekharkhedekar <shekhar.khedekar@gmail.com>
2025-05-23 13:07:00 -0700

    add plugin

--- expoexample/eslint.config.js
+++ expoexample/eslint.config.js
@@ -1,6 +1,7 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const prettierPlugin = require('eslint-plugin-prettier');
 
 module.exports = defineConfig([
   expoConfig,
